031c54c4dc42170db04caf8f31871d4efa96614f
Allen Olsen <allenolsen@quickenloans.com>
2019-03-14 13:57:56 -0400

    First demo

--- 02/demos/after/PeopleViewer/App.xaml.cs
+++ 02/demos/after/PeopleViewer/App.xaml.cs
@@ -1,9 +1,24 @@
-﻿using System.Windows;
+﻿using PeopleViewer.Presentation;
+using PersonDataReader.Service;
+using System.Windows;
 
 namespace PeopleViewer
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ComposeObjects();
+            Current.MainWindow.Title = "With Dependency Injection";
+            Current.MainWindow.Show();
+        }
 
+        private static void ComposeObjects()
+        {
+            var reader = new ServiceReader();
+            var viewModel = new PeopleViewModel(reader);
+            Current.MainWindow = new PeopleViewerWindow(viewModel);
+        }
     }
 }
